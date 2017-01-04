f5f7eb66ba143ccde18c2c75587ee9a4c58d74e1
Michael Dyrynda <michael@dyrynda.com.au>
2017-01-04 12:20:59 +1030

    :white_check_mark: Add test to ensure exception is thrown for non-existent card type in static call

--- tests/StripeTestTokenTest.php
+++ tests/StripeTestTokenTest.php
@@ -13,6 +13,14 @@ class StripeTestTokenTest extends PHPUnit_Framework_TestCase
     }
 
     /** @test */
+    public function it_throws_an_exception_when_creating_a_token_with_a_non_existent_card_type_via_static_access()
+    {
+        $this->setExpectedException(\BadMethodCallException::class);
+
+        StripeTestToken::someNonExistentCardType();
+    }
+
+    /** @test */
     public function it_returns_a_valid_visa_card_number()
     {
         $this->assertSame(4012888888881881, StripeTestToken::getCardNumber('validVisa'));

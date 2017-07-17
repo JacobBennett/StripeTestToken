1f8c26483c968aa6bcdbc3140eedbe0878eea312
Jesse Schutt <jesse.schutt@zaengle.com>
2017-07-17 14:27:30 -0500

    Adds optional card information
    Signed-off-by: Jesse Schutt <jesse.schutt@zaengle.com>

--- src/StripeTestToken.php
+++ src/StripeTestToken.php
@@ -20,19 +20,24 @@ class StripeTestToken
 
     public static function __callStatic($method, $args)
     {
-        return self::create($method);
+        return self::create($method, $args);
     }
 
-    public static function create($type)
+    public static function create($type, $args = [])
     {
         return Token::create([
             'card' => [
-                'number'        => self::getCardNumber($type),
-                'exp_month'     => 1,
-                'exp_year'      => date("Y") + 1,
-                'address_line1' => '33 Zonda Lane',
-                'address_zip'   => '44883',
-                'cvc'           => '123',
+                'number'          => self::getCardNumber($type),
+                'exp_month'       => 1,
+                'exp_year'        => date("Y") + 1,
+                'name'            => isset($args[0]['name']) ? $args[0]['name'] : '',
+                'address_line1'   => isset($args[0]['address_line1']) ? $args[0]['address_line1'] : '33 Zonda Lane',
+                'address_line2'   => isset($args[0]['address_line2']) ? $args[0]['address_line2'] : '',
+                'address_city'    => isset($args[0]['address_city']) ? $args[0]['address_city'] : 'Zondaville',
+                'address_state'   => isset($args[0]['address_state']) ? $args[0]['address_state'] : 'AR',
+                'address_zip'     => isset($args[0]['address_zip']) ? $args[0]['address_zip'] : '44883',
+                'address_country' => isset($args[0]['address_country']) ? $args[0]['address_country'] : 'US',
+                'cvc'             => '123',
             ]
         ])->id;
     }

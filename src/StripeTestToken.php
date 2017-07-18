8c7eba1eda4e282fb6110092c29f7ec3a6c4964b
Jacob Bennett <Jacob.J.Bennett@gmail.com>
2017-07-18 00:43:11 -0500

    array_merge maybe?
    Not perfect here, but seems like a tiny bit cleaner solution... cleaner is subjective I suppose. This would allow you to set defaults, and merge in your own values if you wanted by passing them through as an array for the first argument when calling the static method.

--- src/StripeTestToken.php
+++ src/StripeTestToken.php
@@ -20,25 +20,20 @@ class StripeTestToken
 
     public static function __callStatic($method, $args)
     {
-        return self::create($method, $args);
+        return self::create($method, isset($args[0]) ? $args[0] : []);
     }
 
     public static function create($type, $args = [])
     {
         return Token::create([
-            'card' => [
-                'number'          => self::getCardNumber($type),
-                'exp_month'       => 1,
-                'exp_year'        => date("Y") + 1,
-                'name'            => isset($args[0]['name']) ? $args[0]['name'] : '',
-                'address_line1'   => isset($args[0]['address_line1']) ? $args[0]['address_line1'] : '33 Zonda Lane',
-                'address_line2'   => isset($args[0]['address_line2']) ? $args[0]['address_line2'] : '',
-                'address_city'    => isset($args[0]['address_city']) ? $args[0]['address_city'] : 'Zondaville',
-                'address_state'   => isset($args[0]['address_state']) ? $args[0]['address_state'] : 'AR',
-                'address_zip'     => isset($args[0]['address_zip']) ? $args[0]['address_zip'] : '44883',
-                'address_country' => isset($args[0]['address_country']) ? $args[0]['address_country'] : 'US',
-                'cvc'             => '123',
-            ]
+            'card' => array_merge([
+                'number'        => self::getCardNumber($type),
+                'exp_month'     => 1,
+                'exp_year'      => date("Y") + 1,
+                'address_line1' => '33 Zonda Lane',
+                'address_zip'   => '44883',
+                'cvc'           => '123',
+            ], $args)
         ])->id;
     }
 
